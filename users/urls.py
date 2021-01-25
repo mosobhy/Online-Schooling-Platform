9a5d5c55a1eea687f8f7cd14ccd75eae5d8a24e1
mosobhy <mosobhy@debian>
2021-01-25 23:27:02 +0200

    start views

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import  path
+from . import views
+
+
+# the url patters
+urlpatters = [
+    path('login/', views.login)
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    
+    pass
+
+def register_as_instructor(request):
+    pass
+
+def register_as_student(request):
+    pass
+
+# other views
