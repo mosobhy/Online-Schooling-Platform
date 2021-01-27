8b194a18fbf22019d2f54619e66d37b1dd46775f
ahlam-islam <01146364974ah@gmail.com>
2021-01-27 18:49:20 +0200

    update

--- users-web/src/Component/Landing/Login/index.js
+++ users-web/src/Component/Landing/Login/index.js
@@ -1,18 +1,20 @@
 import react, { Component } from "react";
-import { Link } from "react-router-dom";
+
 import './style.css';
 import { FiUserCheck } from "react-icons/fi";
 import { RiLockPasswordLine } from "react-icons/ri";
 
+
+
 class Login extends Component{
     render() {
         return (
-            <div className="home-parent" style={{ backgroundImage: `url('/image/bg-1.jpg')` }}>
-            <div className="layer"></div>
+            <div className="home-parent">
+            
             <div className="doctor-form">
                 <img src="/image/login-5.jpg" className="img-fluid rounded-circle" width="70" height="60" />
-                <h4>Log in</h4>
-                <form >
+                <h4>Welcome back...</h4>
+                <form  >
              
                     <div>
                         <FiUserCheck />
